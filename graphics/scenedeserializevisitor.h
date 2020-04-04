27e6d1d5d2376c34fc799aebef931f0e6685a7ec
tom.wimmenhove@gmail.com <tom.wimmenhove@gmail.com>
2020-04-04 01:36:07 +0100

    Tiny neatification

--- graphics/scenedeserializevisitor.h
+++ graphics/scenedeserializevisitor.h
@@ -20,17 +20,18 @@ public:
 
     void Visit(SceneElement<T>& element) override
     {
-        std::array<NodeFactory, 2> nodeFactories {
-            NodeFactory("Node", []() { return new NodeElement<T>(); }), // First, because LineElements depends   on it
-            NodeFactory("Line", []() { return new LineElement<T>(); }),
-        };
+        std::array<QPair<QString, std::function<DrawingElement<T>*(void)>>, 2> nodeFactories
+        {{
+            { "Node", []() { return new NodeElement<T>(); } }, // First, because LineElements depends on it
+            { "Line", []() { return new LineElement<T>(); } },
+        }};
 
         for(auto& nodeFactory: nodeFactories)
         {
-            auto nodeXmlElements = domElement.elementsByTagName(nodeFactory.name);
+            auto nodeXmlElements = domElement.elementsByTagName(nodeFactory.first);
             for(int i = 0; i < nodeXmlElements.count(); i++)
             {
-                QSharedPointer<DrawingElement<T>> nodeElement = QSharedPointer<DrawingElement<T>>(nodeFactory.factory());
+                QSharedPointer<DrawingElement<T>> nodeElement = QSharedPointer<DrawingElement<T>>(nodeFactory.second());
                 QDomElement nodeXmlElement = nodeXmlElements.item(i).toElement();
                 SceneDeserializeVisitor visitor(nodeXmlElement, nodeMap);
                 nodeElement->Accept(visitor);
@@ -43,16 +44,6 @@ private:
     QMap<int, SharedNode> defaultMap;
     QDomElement& domElement;
     QMap<int, SharedNode>& nodeMap = defaultMap;
-
-    struct NodeFactory
-    {
-        NodeFactory(const QString& name, const std::function<DrawingElement<T>*(void)>& factory)
-            : name(name), factory(factory)
-        { }
-
-        QString name;
-        std::function<DrawingElement<T>*(void)> factory;
-    };
 
     SceneDeserializeVisitor(QDomElement& domElement, QMap<int, SharedNode>& nodeMap)
         : domElement(domElement), nodeMap(nodeMap)
